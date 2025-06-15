33fd7df20164f8646fb174b9da3ad16df7662dee
alfin0431 <alfinnn831@gmail.com>
2025-06-15 14:08:10 +0700

    Fix Bug (1)

--- ProjekUAS_S3hyper/database/seeders/DatabaseSeeder.php
+++ ProjekUAS_S3hyper/database/seeders/DatabaseSeeder.php
@@ -2,22 +2,26 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $this->call([
+            RoleSeeder::class,
+            UserSeeder::class,
+            KategoriSeeder::class,
+            RakSeeder::class,
+            PenerbitSeeder::class,
+            BukuSeeder::class,
+            TransaksiSeeder::class,
+            PeminjamanSeeder::class,
+        ]);;
     }
 }
